b69c7e347bb06724bed7e07130449c3566d56bb9
Emmanuel Daniel <emmsdan@users.noreply.github.com>
2018-06-28 12:06:51 +0100

    Update serviceWorker.js

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,36 +1,27 @@
-const cacheVersion = '0.2';
+const cacheVersion = '0.1';
 const cacheName = 'currencyConverter.io';
 const cachNameVersion= `${cacheName}-${cacheVersion}`;
-
-//const Controller = new PageController();
 
 const cachableAPI = [
   'https://free.currencyconverterapi.com/api/v5/currencies',
   'https://free.currencyconverterapi.com/api/v5/countries'
 ];
 
-/*
-const cachableAPI = [
-  './api/countries.json',
-  './api/currencies.json'
-];
-*/
 const cachableFiles = [
   './',
   './img/currency_icon.png',
   './index.html',
   './css/currency.css',
-  './js/PageController.js',
-  './js/converter.js',
-  './js/calculator.js'
+  './js/controller.js',
+  './js/converter.js'
 ]
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    //store cache first time
+    //get cache first time
     caches.open(cachNameVersion)
     .then((cache) => {
-      cache.addAll(cachableFiles.concat(cachableAPI));
+      return cache.addAll(cachableFiles.concat(cachableAPI))
     })
   );
 });
@@ -41,34 +32,44 @@ self.addEventListener('activate', (event) => {
     caches.keys()
     .then( (keys) => {
         return Promise.all(keys.map((key, i) => {
-            if(key !== cachNameVersion){
-                return caches.delete(keys[i]);
-            }
-        }))
-    })
+          if(key !== cachNameVersion){
+            return caches.delete(keys[i]);
+          }
+      }))
+    })  
   )
 });
 
 self.addEventListener('fetch', (event) => {
-  const url = new URL(event.request.url);
+  let url = event.request.clone();
+
   event.respondWith(
     caches.match(event.request)
-    .then((response) => {
-      return false;
-      // check for request exist and return request on true
-      if (response)  return response;
-      // check server for request if not exist
-      let url = event.request.clone();
-  fetch(url)
-  .then((response)=>{
-      if (response || response.status == 200 || response == 'basic') return res;
-
-      caches.open (cachNameVersion)
-  .then((cache) => {
-    cache.put(event.request, response.clone());
-  })
-      return response;
-  })
+    .then((res) => {
+      if(res){
+        return res;
+      }
+      return fetch(url)
+      .then((res) => {        
+        if(!res || res.status !== 200 || res.type !== 'basic'){
+          return res;
+        }
+        let response = res.clone();
+        caches.open(cachNameVersion)
+        .then((cache) => {
+          cache.put(event.request, response);
+        });
+        return res;
+      })
     })
-  );
+  )
 });
+
+self.addEventListener('message', messageEvent => {
+  if (messageEvent.data === 'skipWaiting') return skipWaiting();
+});
+
+self.addEventListener('controllerchange', () => {
+  console.log('sdadasasd')
+    window.location.assign('./');
+})
