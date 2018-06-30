a034233aee1f6d3c948fb37486ea05bf0a06c27c
Emmanuel Daniel <emmsdan@users.noreply.github.com>
2018-06-30 22:29:26 +0100

    Update serviceWorker.js

--- serviceWorker.js
+++ serviceWorker.js
@@ -4,7 +4,7 @@
   * date @2018-june
 */
 
-const cacheVersion = '1.0';
+const cacheVersion = '0.0';
 const cacheName = 'currencyConverter.io';
 const cachNameVersion= `${cacheName}-${cacheVersion}`;
 
