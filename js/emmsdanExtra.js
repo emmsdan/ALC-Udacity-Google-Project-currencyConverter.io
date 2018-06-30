e1aa3d5a9619517de191c5e79f6fbefe388e78c0
Emmanuel Daniel <emmsdan@users.noreply.github.com>
2018-06-30 21:53:25 +0100

    Update emmsdanExtra.js

--- js/emmsdanExtra.js
+++ js/emmsdanExtra.js
@@ -1,12 +1,18 @@
+/* 
+  * author @emmsdan
+  * for @alc 3.0
+  * date @2018-june
+*/
+
+/* a little toast, won't kill */
 const toast = (msg, varibles= null) => {
   
   let toastContainer = document.querySelector("#snackbar");
   toastContainer.setAttribute('class', colors())
   toastContainer.innerHTML = `<span> ${msg} </span>`;
-  if(varibles != null) {
-    toastContainer.innerHTML += `<a href='./?'> Update </a>`;
-  }
-
+  
+  varibles != null ? toastContainer.innerHTML += `<a href='./?'> Update </a>`;
+  
   toastContainer.classList.add('show');
   
   const removeToast = () => {
