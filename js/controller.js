21c01a550bd6e8db2766ad3479d799a7b2bf90fb
Emmanuel Daniel <emmsdan@users.noreply.github.com>
2018-06-30 21:51:22 +0100

    Update controller.js

--- js/controller.js
+++ js/controller.js
@@ -1,50 +1,52 @@
+/* 
+  * author @emmsdan
+  * for @alc 3.0
+  * date @2018-june
+*
 /* naming and settings for indexDB */
 let dbname = {exchange: '', currency: ''}
 
-// for browser compactibility
-const dBase = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB || window.shimIndexedDB;
-
-window.IDBTransaction = window.IDBTransaction || window.webkitIDBTransaction || window.msIDBTransaction;
-window.IDBKeyRange = window.IDBKeyRange || window.webkitIDBKeyRange || window.msIDBKeyRange;
-localIndexStorage = window.LocalIndexedStorage;
-
-
-/* service worker */
 const startDb = () => {
-  // Check for service worker and IndexedDB
+// for browser compactibility
+  const dBase = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB || window.shimIndexedDB;
+  window.IDBTransaction = window.IDBTransaction || window.webkitIDBTransaction || window.msIDBTransaction;
+  window.IDBKeyRange = window.IDBKeyRange || window.webkitIDBKeyRange || window.msIDBKeyRange;
   localIndexStorage = window.LocalIndexedStorage;
-}
 
-const registerServiceWorker = () => {
+  // Check for service worker and IndexedDB
   if (!navigator.serviceWorker || !window.LocalIndexedStorage){ 
-    toast('Service Worker not supported');
+    toast('Your Browser is a little outdated. Please Make a date with the latest Browser to browse offline');
     return Promise.resolve();
   }
-  navigator.serviceWorker.register('./serviceWorker.js', {scope: './'})
-  .then((registration) => {
-  console.log('welcome home again');
-      // checking if controller is true/false
-      if (!navigator.serviceWorker.controller) return;
 
-      if(registration.waiting){
-        toast('New Version Available', 'updateServiceWorker');
-        registration.waiting.postMessage('skipWaiting');
-        return;
-      }
-      if (registration.installing){
-        serviceWorkerInstallation(registration.installing);
-        return;
-      }
 
-      registration.addEventListener('updatefound', () => {
-        serviceWorkerInstallation(registration.installing);
-        return;
-      });
-  })
-  .catch((err) => {
-    console.log(`Error: SR2312, ${err}`)
-  })
+  const registerServiceWorker = () => {
+    navigator.serviceWorker.register('./serviceWorker.js', {scope: './'})
+    .then((registration) => {
+        // checking if controller is available or free
+        if (!navigator.serviceWorker.controller) return;
+
+        if(registration.waiting){
+          toast('New Version Available', 'updateServiceWorker');
+          registration.waiting.postMessage('skipWaiting');
+          return;
+        }
+        if (registration.installing){
+          serviceWorkerInstallation(registration.installing);
+          return;
+        }
+        registration.addEventListener('updatefound', () => {
+          serviceWorkerInstallation(registration.installing);
+          return;
+        });
+    })
+    .catch((err) => {
+      console.log(`Error: SR2312, ${err}`)
+    })
+  }
 }
+
+/* check user is using the latest version */
 const serviceWorkerInstallation = (status) =>{
   status.addEventListener('statechange', () => {
     if (status.state == 'installed'){
@@ -53,6 +55,13 @@ const serviceWorkerInstallation = (status) =>{
   })
 }
 
+/*
+  the following two functions are for online conversion of currencies.
+    they both  go online to get requested exchange rate and store it in the indexdb
+    the difference is.
+        * onlineConvertion(): access the requested currencies directly while
+        * onlineReverseConvertion() : access the requested currencies in a reverse order.
+*/
 const onlineConvertion = () => {
   let fromCurrency = getFromCurrency();;
   let toCurrency = getToCurrency();
@@ -63,18 +72,19 @@ const onlineConvertion = () => {
   })
   .then ((jsonResponse)=>{
     const currency = jsonResponse[`${toCurrency}_${fromCurrency}`];
-    
+    //display exchange rate.
       displayExchangeRate({rate :conversion(currency, getAmount()), date: getDate()})
+    // go online again and get the reserve for future use.
       onlineReverseConvertion(getFromCurrency(), getToCurrency());
-      
+    //now store the previos exchange rate into database
       storeIntoDatabase(`${toCurrency}_${fromCurrency}`, currency);
       return; 
   })
   .catch ((e)=>{
-    let message = 'Oops, connection issues. Reconnecting...'
-    toast(message);
+    toast('Oops, connection issues. Reconnecting...');
   })
 }
+
 const onlineReverseConvertion = (fromCurrency, toCurrency) => {
 
   fetch (getAPIUrl(fromCurrency, toCurrency))
@@ -86,9 +96,11 @@ const onlineReverseConvertion = (fromCurrency, toCurrency) => {
       storeIntoDatabase(`${fromCurrency}_${toCurrency}`, currency);
   })
   .catch ((e)=>{
-    toast(e);
+    toast('Error Connecting to the server. try again...');
   })
 }
+
+/* get and store exchange rate into IndexDB */
 const storeIntoDatabase = (conversions, amount) => {
   return localIndexStorage.open()
   .then((idb) => {
